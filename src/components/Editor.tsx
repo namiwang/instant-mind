0071bd26aacdc3af1ed64441a121d6baf79a2a2b
Nami.W <namiheike@gmail.com>
2021-06-17 23:46:02 +0800

    editor toggle and focus

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import { useRoot } from "../models/root"
 
-const Editor = () => {
+const INIT_RAW = '- mind'
+
+const Editor = ({textareaRef}: {textareaRef: React.RefObject<HTMLTextAreaElement>}) => {
   const root = useRoot()
 
+  const [raw, updateRaw] = useState(INIT_RAW)
+
   function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>): void {
-    root.updateRaw(event.target.value)
+    const value = event.target.value
+    updateRaw(value)
+    root.updateRaw(value)
   }
 
   return (
-    <textarea id="editor" onInput={handleChange}></textarea>
+    <textarea id="editor" ref={textareaRef} value={raw} onInput={handleChange}></textarea>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
-import Editor from '../components/Editor';
-import Mind from '../components/Mind';
-import './Home.sass';
+import { IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react'
+import { pencilOutline, text } from 'ionicons/icons'
+import { useEffect, useRef, useState } from 'react'
+import Editor from '../components/Editor'
+import Mind from '../components/Mind'
+import './Home.sass'
 
 const Home: React.FC = () => {
+  const [showEditor, setShowEditor] = useState(true)
+
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  const focusOnEditor = () => {
+    textareaRef.current?.focus()
+    // TODO cursor pos
+  }
+
+  const toggleShowEditor = () => {
+    setShowEditor(!showEditor)
+    if (showEditor) {
+      focusOnEditor()
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(focusOnEditor, 500)
+  }, [])
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,7 +40,12 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
-        <Editor />
+        <IonFab vertical="top" horizontal="start" slot="fixed" >
+          <IonFabButton onClick={toggleShowEditor}>
+            <IonIcon icon={pencilOutline} />
+          </IonFabButton>
+        </IonFab>
+        { showEditor ? <Editor textareaRef={textareaRef} /> : null }
         <Mind />
       </IonContent>
     </IonPage>
