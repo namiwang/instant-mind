e91cc26162919ea3c6cb032fbfad8bab3bc7a180
Nami.W <namiheike@gmail.com>
2021-06-20 10:31:43 +0800

    Updates

---
+++ static/js/53.1c6df0dd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpinstant-mind"]=this["webpackJsonpinstant-mind"]||[]).push([[53],{436:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return c}));var e=i(23),o=i(21),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,i=n.innerHeight,c=document.elementFromPoint(t/2,i/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(e.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.1c6df0dd.chunk.js.map
--- static/js/53.791a8c7f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpinstant-mind"]=this["webpackJsonpinstant-mind"]||[]).push([[53],{455:function(n,t,i){"use strict";i.r(t),i.d(t,"startStatusTap",(function(){return c}));var e=i(24),o=i(22),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.h)((function(){var t=n.innerWidth,i=n.innerHeight,c=document.elementFromPoint(t/2,i/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(e.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.791a8c7f.chunk.js.map
